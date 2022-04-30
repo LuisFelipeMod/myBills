6dc668dda0e355b54f58cb5908f495b9f586c816
Luis Modesto <luisfelipetecno@gmail.com>
2022-04-30 01:29:55 -0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-alert("Site em desenvolvimento!");
+//alert("Site em desenvolvimento!");
+
+$(document).ready(function() {
+    $(".divPasswordOcult").bind('click', function(e) {
+        $(".divPasswordOcult svg").css('stroke', 'rgb(255, 255, 255, 0.5)');
+    });
+})
+    
